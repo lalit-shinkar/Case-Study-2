74c7b018f3bf9097f82bfd49dd44e02883689821
lalit-shinkar <shinkarlalit18@gmail.com>
2025-07-31 12:20:47 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -74,6 +74,13 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["13.127.147.252/32"]
   }
 
+ ingress {
+    from_port   = var.app_port
+    to_port     = var.app_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
